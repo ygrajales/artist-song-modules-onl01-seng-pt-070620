148970daf538784ac3b3a797badc1d4ff52d2f3f
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-10 15:51:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,10 +17,6 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@artists
   end
@@ -34,9 +30,6 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
   
   
 end
